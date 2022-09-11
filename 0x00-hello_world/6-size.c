a02b7d5f7ceaef508d3fb68a04e957fa3c5c16ff
Amani Kaneza <akanezamulume.iuea@gmail.com>
2022-09-11 07:43:12 -0700

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Description: C program that print with put function
+ *Description: a C program that print with put function
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main (void)
 {
