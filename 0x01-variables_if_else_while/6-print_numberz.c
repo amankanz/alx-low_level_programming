d6cf6a9e0415fbc4a9e7ef80b10ba4a721563db0
Amani Kaneza <akanezamulume.iuea@gmail.com>
2022-09-12 11:14:53 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - c main fxn
+ * num: number
+ * Return: main returns zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+		putchar(num % 10 + '0');
+	putchar('\n');
+	return (0);
