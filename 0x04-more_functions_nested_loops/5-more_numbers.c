e1c78d28919067ac23d64b579b63afe026b57116
Amani Kaneza <akanezamulume.iuea@gmail.com>
2022-09-23 05:10:53 -0700

    Update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,28 @@
-#include <stdio.h>
-#include "main.h"
+nclude"main.h"
+
 /**
- * more_numbers - prints numbers
+ * more_numbers - print 0 - 14 ten times and you
+ *		can only use _putchar three times
  *
- */
+ * Return: Always 0 (Success)
+*/
+
 void more_numbers(void)
 {
-	int i, j;
+	int num, row, count;
 
-	i = 0;
-	while (i < 10)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (j = 0; j <= 14; j++)
-			printf("%d", j);
-		putchar('\n');
-		++i;
+		for (count = 0; count <= 14; ++count)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
 	}
-	putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
-				putchar(' ');
-			putchar(92);
-			putchar('\n');
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
 		}
 	}
 }
